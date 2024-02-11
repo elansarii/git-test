c95fe62c022156e86e899eea85eacca7cf6c5324
ansari <elansari1211@gmail.com>
2024-02-11 13:28:48 +0300

    version 2

--- math.py
+++ math.py
@@ -4,5 +4,11 @@ def fact(n):
     else:
         return n*fact(n-1)
 
+def fib(n):
+    if(n<=1):
+        return n
+    else:
+        return fib(n-1)+fib(n-2)
 
 print("Factorial is: ", fact(3))
+print("Fib is: ", fib(3))
